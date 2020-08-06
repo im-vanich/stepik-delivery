95bf02543928b1179ef653a6950510ee73f38a4d
Ivan Matveev <skiforceone@gmail.com>
2020-08-06 17:51:48 +0300

    Изменил структуру базы даннах, наполнил данными таблицу с категориями и блюдами

--- models.py
+++ models.py
@@ -4,39 +4,29 @@ db = SQLAlchemy()
 
 
 class User(db.Model):
-    __tabelname__ = 'users'
-
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String, nullable=False, unique=True)
-    password = db.Column(db.String, nullable=False, unique=True)
-    order_id = db.Column(db.Integer, db.ForeignKey('orders.id'))
-    order = db.relationship('Order')
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(20), nullable=False, unique=True)
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'))
+    orders = db.relationship('Order', backref='orders')
 
 
 class Dish(db.Model):
-    __tablename__ = 'dishes'
-
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False, unique=True)
     price = db.Column(db.Float, nullable=False)
     description = db.Column(db.String, nullable=False, unique=True)
     picture = db.Column(db.String, nullable=False, unique=True)
-    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
-    category = db.relationship('Category')
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
 
 
 class Category(db.Model):
-    __tablename__ = 'categories'
-
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False, unique=True)
-    dishes_id = db.Column(db.Integer, db.ForeignKey('dishes.id'))
     dish = db.relationship('Dish')
 
 
 class Order(db.Model):
-    __tablename__ = 'orders'
-
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date, nullable=False)
     order_price = db.Column(db.Float, nullable=False)
@@ -44,4 +34,4 @@ class Order(db.Model):
     email = db.Column(db.String, nullable=False, unique=True)
     phone = db.Column(db.String, nullable=False, unique=True)
     address = db.Column(db.String, nullable=False, unique=True)
-    dishes_list = db.Column(db.String, nullable=False, unique=True)
+    dishes_list = db.relationship('Dish', backref='dishes')
